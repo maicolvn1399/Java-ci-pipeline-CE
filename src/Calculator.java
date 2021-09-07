b5f59b9749b0f889a30bdbad12d32e616688d213
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2021-09-06 18:54:42 -0600

    [Created] Calculator code have been created

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public double sum(double a, double b) {
+        double res = a + b;
+        return res;
+    }
+
+    public double res(double a, double b) {
+        double res = a - b;
+        return res;
+    }
+
+    public double div(double a, double b) {
+        double res = a / b;
+        return res;
+    }
+
+    public double mult(double a, double b) {
+        double res = a*b;
+        return res;
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1, 3));
+        System.out.println(calc.res(10, 3));
+        System.out.println(calc.mult(9, 9));
+        System.out.println(calc.div(-5, 0));
+    }
+
+}
